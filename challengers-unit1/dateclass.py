b55f98b7680d2505f5a6144fd86db010a4787cd5
attitudekingkiran <144333141+attitudekingkiran@users.noreply.github.com>
2023-09-07 15:39:07 +0530

    Add files via upload

---
+++ challengers-unit1/dateclass.py
@@ -0,0 +1,16 @@
+import datatime
+today=datatime.data.today()
+print(today)
+print('ctime:',today.ctime())
+print('year:',today,year)
+print('mon:',today,month)
+print('day:',today,day)
+
+
+
+
+
+
+
+
+           
---
+++ challengers-unit1/leepyear.py
@@ -0,0 +1,9 @@
+def leepyear(x):
+    if x%4==0:print("The given year",x,"is leep year")
+    else:print("The given year",x,"is not a leep year")
+year=int(input("enter a year"))
+leepyear(year)
+              
+
+                        
+                  
